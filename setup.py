b38705ed64b0c70d77f52fe16a5929cb37ffddf8
pharesim <pharesim@protonmail.com>
2020-04-16 17:49:22 +0200

    version 0.9.3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from setuptools.command.test import test as TestCommand
 
 # Package meta-data.
 NAME = 'hivepy'
-VERSION = '0.9.2'
+VERSION = '0.9.3'
 DESCRIPTION = 'A python hive library.'
 URL = 'https://github.com/pharesim/hive-python'
 EMAIL = 'pharesim@protonmail.com'
